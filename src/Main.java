e81cdec56c057b80bd5a4d679d73d36af27d672a
Ridireacht <ridiredire@gmail.com>
2023-08-12 17:32:28 +0300

    Set up basic program structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        boolean toContinue = true;
+
+        ConsoleIO.SetBalance(1337);
+
+
+        do
+        {
+            ConsoleIO.ShowMenu("Возможные опции: создать счёт (1), войти в счёт (2), выйти (3).");
+
+            switch(ConsoleIO.GetOption())
+            {
+                case(1):
+                {
+                    ConsoleIO.CreateClient();
+                    break;
+                }
+
+                case(2):
+                {
+                    break;
+                }
+
+                case(3):
+                {
+                    toContinue = false;
+                    break;
+                }
+            }
+
+        } while(toContinue)
     }
 }
