11010ab86df5fa5c4e40b3513e1b6630c5984b50
Ridireacht <ridiredire@gmail.com>
2023-08-12 12:59:35 +0300

    Add basic GetPIN() method

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -5,7 +5,7 @@ public class ConsoleIO
 {
     public static int GetOption()
     {
-        int res = 0;
+        int res;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Введите ваш выбор: ");
@@ -34,5 +34,42 @@ public class ConsoleIO
 
         sc.close();
         return res;
+    }
+
+
+    public static boolean GetPIN(int realPIN)
+    {
+        int res;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Введите ПИН-код: ");
+
+
+        // На угадывание PIN даётся 3 попытки
+        for (int i = 1; i < 4; i++)
+        {
+            // Перебираем входной поток, пока не найдём там int
+            while(!sc.hasNextInt())
+            {
+                System.out.println("\nНекорректное значение! Попробуйте снова.");
+                sc.next();
+            }
+
+
+            res = sc.nextInt();
+
+            if (res == realPIN)
+            {
+                sc.close();
+                return true;
+            }
+
+            else
+                System.out.println("\nНеправильный ПИН-код! Осталось попыток: " + (3 - i));
+        }
+
+
+        System.out.println("\nПИН-код был неверно введён 3 раза, банкомат заблокирован.");
+        return false;
     }
 }
