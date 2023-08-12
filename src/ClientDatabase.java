04fa9fa2ca0ca7278d0b30239b5de7b29ec7e583
Ridireacht <ridiredire@gmail.com>
2023-08-12 18:39:58 +0300

    Add list of card numbers, add method to check for them

--- src/ClientDatabase.java
+++ src/ClientDatabase.java
@@ -3,5 +3,12 @@ import java.util.ArrayList;
 
 public class ClientDatabase
 {
-    public static ArrayList<Client> clients = new ArrayList<Client>();
+    private static ArrayList<Client> clients = new ArrayList<Client>();
+    private static ArrayList<Long> cardNumbers = new ArrayList<Long>();
+
+
+    public static boolean CardNumberExists(long number)
+    {
+        return cardNumbers.contains(number) ? true : false;
+    }
 }
