2211b06cab70de69badc6128a08b1ed619e5adfc
Ridireacht <ridiredire@gmail.com>
2023-08-14 19:18:19 +0300

    Remove separate list for card numbers, check card numbers straight in clients list

--- src/ClientDatabase.java
+++ src/ClientDatabase.java
@@ -4,20 +4,10 @@ import java.util.ArrayList;
 public class ClientDatabase
 {
     private static ArrayList<Client> clients = new ArrayList<Client>();
-    private static ArrayList<Long> cardNumbers = new ArrayList<Long>();
 
 
     public static boolean CardNumberExists(long number)
     {
-        return cardNumbers.contains(number) ? true : false;
-    }
-
-
-    public static void UpdateCardNumbersList()
-    {
-        cardNumbers.clear();
-
-        for(var client : clients)
-            cardNumbers.add(client.cardNumber);
+        return clients.stream().anyMatch(o -> o.cardNumber == number);
     }
 }
