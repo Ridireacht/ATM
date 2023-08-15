1a748a41191a7c49c964d5fce684a91e3e082cd9
Ridireacht <ridiredire@gmail.com>
2023-08-15 20:47:58 +0300

    Move some content to other class

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -1,337 +1,147 @@
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 
-public class ConsoleIO
-{
-    public static int balance;
-    private static Client currentClient = new Client();
-    private static final Scanner sc = new Scanner(System.in);
+public class ConsoleIO {
+
+  private static final Scanner sc = new Scanner(System.in);
 
 
+  private static void clear() {
+    try {
+      String os = System.getProperty("os.name");
 
-    private static void clear()
-    {
-        try
-        {
-            String os = System.getProperty("os.name");
+      if (os.contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      }
 
-            if (os.contains("Windows"))
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      else {
+        Runtime.getRuntime().exec("clear");
+      }
 
-            else
-                Runtime.getRuntime().exec("clear");
-        }
+    } catch (Exception e) {
+      System.out.println("При попытке очистить экран возникла непредвиденная ошибка!\n");
+    }
+  }
 
-        catch (Exception e)
-        {
-            System.out.println("При попытке очистить экран возникла непредвиденная ошибка!\n");
-        }
+
+  public static void pressEnterToExit() {
+    try {
+      System.in.read();
     }
 
+    catch (IOException e2) {
+      System.out.println("\nПри попытке считать нажатие Enter возникла непредвиденная ошибка!");
+    }
+  }
 
-    public static void pressEnterToExit()
-    {
-        try { System.in.read(); }
-        catch (IOException e2) { System.out.println("\nПри попытке считать нажатие Enter возникла непредвиденная ошибка!"); }
+
+  public static void showHeader(int balance) {
+    ConsoleIO.clear();
+
+    System.out.println("////////////////////////////////////");
+    System.out.println("/             БАНКОМАТ             /");
+    System.out.println("////////////////////////////////////");
+
+    System.out.println("\n Баланс банкомата: " + balance);
+    System.out.println("____________________________________\n\n\n");
+  }
+
+
+  public static void showMainMenu(int balance) {
+    showHeader(balance);
+
+    System.out.println("           ГЛАВНОЕ МЕНЮ\n");
+    System.out.println("Возможные опции:\n1. Создать счёт\n2. Войти в счёт\n3. Завершить работу\n");
+    System.out.println("Введите номер интересующей опции:");
+  }
+
+
+  public static void showAccount(int balance, Client client) {
+    showHeader(balance);
+
+    System.out.println("           АККАУНТ\n");
+    System.out.println("Здравствуйте, " + client.FIO + "!");
+    System.out.println("\nБаланс на вашем счету: " + client.accountBalance);
+    System.out.println("\nВыберите дальнейшее действие: внести средства (1), снять средства (2), выйти из аккаунта (3).");
+  }
+
+
+  // Для ввода номера карты
+  public static long getOption(long lower_boundary, long upper_boundary) {
+    long input;
+
+    while (true) {
+      while (!sc.hasNextLong()) {
+        System.out.println("\nНекорректное значение! Попробуйте снова.");
+        sc.next();
+      }
+
+      input = sc.nextLong();
+
+      if (input >= lower_boundary && input <= upper_boundary) {
+        break;
+      }
+
+      else {
+        System.out.println("\nНомер карты должен состоять из 16 цифр! Попробуйте снова.");
+      }
     }
 
+    sc.nextLine();  // чтобы убрать \n, которое осталось после nextLong()
 
-    private static void showHeader()
-    {
-        ConsoleIO.clear();
+    return input;
+  }
 
-        System.out.println("////////////////////////////////////");
-        System.out.println("/             БАНКОМАТ             /");
-        System.out.println("////////////////////////////////////");
 
-        System.out.println("\n Баланс банкомата: " + balance);
-        System.out.println("____________________________________\n\n\n");
+  // Для ввода обычных чисел (опций, сумм, ПИН-кода)
+  public static int getOption(int lower_boundary, int upper_boundary, String errorMessage) {
+    int input;
+
+    while (true) {
+      while (!sc.hasNextInt()) {
+        System.out.println("\nНекорректное значение! Попробуйте снова.");
+        sc.next();
+      }
+
+      input = sc.nextInt();
+
+      if (input >= lower_boundary && input <= upper_boundary) {
+        break;
+      }
+
+      else {
+        System.out.println("\n" + errorMessage);
+      }
     }
 
+    sc.nextLine();  // чтобы убрать \n, которое осталось после nextInt()
 
-    public static void showMainMenu()
-    {
-        showHeader();
+    return input;
+  }
 
-        System.out.println("           ГЛАВНОЕ МЕНЮ\n");
-        System.out.println("Возможные опции:\n1. Создать счёт\n2. Войти в счёт\n3. Завершить работу\n");
-        System.out.println("Введите номер интересующей опции:");
+
+  // Для ввода ФИО
+  public static String getOption() {
+    String input;
+
+    while (true) {
+      input = sc.nextLine();
+
+      if (input.matches("[а-яА-Я ]+")) {
+        break;
+      }
+
+      else {
+        System.out.println("\nНекорректное значение! Попробуйте снова.");
+      }
     }
 
+    return input;
+  }
 
-    private static void showAccount()
-    {
-        showHeader();
 
-        System.out.println("           АККАУНТ\n");
-        System.out.println("Здравствуйте, " + currentClient.FIO + "!");
-        System.out.println("\nБаланс на вашем счету: " + currentClient.accountBalance);
-        System.out.println("\nВыберите дальнейшее действие: внести средства (1), снять средства (2), выйти из аккаунта (3).");
-    }
-
-
-    // Для ввода номера карты
-    private static long getOption(long lower_boundary, long upper_boundary)
-    {
-        long input;
-
-
-        while(true)
-        {
-            while(!sc.hasNextLong())
-            {
-                System.out.println("\nНекорректное значение! Попробуйте снова.");
-                sc.next();
-            }
-
-
-            input = sc.nextLong();
-
-            if (input >= lower_boundary && input <= upper_boundary)
-                break;
-
-            else
-                System.out.println("\nНомер карты должен состоять из 16 цифр! Попробуйте снова.");
-        }
-
-
-        sc.nextLine();  // чтобы убрать \n, которое осталось после nextLong()
-
-        return input;
-    }
-
-
-    // Для ввода обычных чисел (опций, сумм, ПИН-кода)
-    public static int getOption(int lower_boundary, int upper_boundary, String errorMessage)
-    {
-        int input;
-
-
-        while(true)
-        {
-            while(!sc.hasNextInt())
-            {
-                System.out.println("\nНекорректное значение! Попробуйте снова.");
-                sc.next();
-            }
-
-
-            input = sc.nextInt();
-
-            if (input >= lower_boundary && input <= upper_boundary)
-                break;
-
-            else
-                System.out.println("\n" + errorMessage);
-        }
-
-
-        sc.nextLine();  // чтобы убрать \n, которое осталось после nextInt()
-
-        return input;
-    }
-
-
-    // Для ввода ФИО
-    private static String getOption()
-    {
-        String input;
-
-
-        while(true)
-        {
-            input = sc.nextLine();
-
-            if (input.matches("[а-яА-Я ]+"))
-                break;
-
-            else
-                System.out.println("\nНекорректное значение! Попробуйте снова.");
-        }
-
-
-        return input;
-    }
-
-
-    private static boolean isCorrectPIN()
-    {
-        int localPIN;
-
-
-        for (int i = 1; i < 4; i++)
-        {
-            localPIN = ConsoleIO.getOption(1000, 9999, "ПИН-код должен состоять из 4 цифр!");
-
-            if (localPIN == currentClient.PIN)
-                return true;
-
-            else if (i != 3)
-                System.out.println("\nНеправильный ПИН-код! Осталось попыток: " + (3 - i));
-        }
-
-
-        return false;
-    }
-
-
-    private static void addMoney()
-    {
-        System.out.println("\nВведите, сколько вы хотите добавить:");
-
-        int sum = ConsoleIO.getOption(1, 1000000, "Сумма пополнения должна быть в пределах 1-1000000!");
-
-        balance += sum;
-        currentClient.accountBalance += sum;
-
-        System.out.println("Счёт успешно пополнен! Для возврата к аккаунту нажмите Enter...");
-
-        ConsoleIO.pressEnterToExit();
-    }
-
-
-    private static void withdrawMoney()
-    {
-        System.out.println("\nВведите, сколько вы хотите cнять:");
-
-        int sum;
-
-        while (true)
-        {
-            sum = ConsoleIO.getOption(1, currentClient.accountBalance,  "Сумма снятия должна быть в пределах 1-" + currentClient.accountBalance + "!");
-
-            if (sum > balance)
-                System.out.println("Снятие невозможно: в банкомате есть всего лишь " + balance + "! Выберите другую сумму.");
-
-            else
-                break;
-        }
-
-        balance -= sum;
-        currentClient.accountBalance -= sum;
-
-        System.out.println("Деньги успешно выведены! Для возврата к аккаунту нажмите Enter...");
-
-        ConsoleIO.pressEnterToExit();
-    }
-
-
-    public static void createClientAccount()
-    {
-        showHeader();
-
-        System.out.println("         CОЗДАНИЕ АККАУНТА\n");
-
-
-        System.out.println("Введите своё ФИО (кириллицей):");
-        currentClient.FIO = ConsoleIO.getOption();
-
-        System.out.println("Введите ПИН-код (должен состоять из 4 цифр): ");
-        currentClient.PIN = ConsoleIO.getOption(999, 10000, "ПИН-код должен состоять из 4 цифр!");
-
-        currentClient.cardNumber = Client.generateCardNumber();
-        currentClient.accountBalance = 0;
-
-
-        ClientDatabase.clients.add(currentClient);
-        System.out.println("\nАккаунт создан! Номер вашей карты: " + currentClient.cardNumber + "\nНажмите Enter, чтобы выйти на главный экран...");
-
-        ConsoleIO.pressEnterToExit();
-    }
-
-
-    public static void enterClientAccount()
-    {
-        showHeader();
-
-        System.out.println("          ВХОД В АККАУНТ\n");
-
-
-        while (true)
-        {
-            System.out.println("Введите номер своей карты в формате XXXXXXXXXXXXXXXX: ");
-            currentClient.cardNumber = ConsoleIO.getOption(1000000000000000L, 9999999999999999L);
-
-            if (ClientDatabase.getClientByCard(currentClient.cardNumber) != null)
-                break;
-
-            else
-                System.out.println("Номер карты не найден! Попробуйте снова.");
-        }
-
-
-
-        currentClient = ClientDatabase.getClientByCard(currentClient.cardNumber);
-
-        if (currentClient.availableSince.after(new Date()))
-        {
-            long diffInMillies = Math.abs(currentClient.availableSince.getTime() - new Date().getTime());
-            long diff = TimeUnit.MINUTES.convert(diffInMillies, TimeUnit.MILLISECONDS);
-
-            System.out.println("Данный аккаунт всё ещё заблокирован!");
-            System.out.println("Время до снятия блокировки: " + diff + " минут.");
-            System.out.println("Нажмите Enter, чтобы выйти в главное меню...");
-
-            ConsoleIO.pressEnterToExit();
-
-            return;
-        }
-
-
-
-        System.out.println("Введите свой ПИН-код (длина - 4 символа): ");
-
-        if (isCorrectPIN())
-        {
-            boolean toContinue = true;
-
-            do
-            {
-                ConsoleIO.showAccount();
-
-                switch (ConsoleIO.getOption(1, 3, "Выбор должен быть от 1 до 3! Попробуйте снова."))
-                {
-                    case (1) ->
-                    {
-                        ConsoleIO.addMoney();
-                        ClientDatabase.updateClient(currentClient);
-                    }
-
-                    case (2) ->
-                    {
-                        ConsoleIO.withdrawMoney();
-                        ClientDatabase.updateClient(currentClient);
-                    }
-
-                    case (3) -> toContinue = false;
-                }
-
-            } while(toContinue);
-        }
-
-        else
-        {
-            currentClient.availableSince = new Date();
-
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(currentClient.availableSince);
-            cal.add(Calendar.DATE, 1);
-            currentClient.availableSince = cal.getTime();
-
-            System.out.println("\nПИН-код был неверно введён 3 раза, аккаунт заблокирован на день.");
-            System.out.println("Нажмите Enter, чтобы выйти в главное меню...");
-
-            ConsoleIO.pressEnterToExit();
-        }
-    }
-
-
-    public static void closeScanner()
-    {
-        sc.close();
-    }
+  public static void closeScanner() {
+    sc.close();
+  }
 }
